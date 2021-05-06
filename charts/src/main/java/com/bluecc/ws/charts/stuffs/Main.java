f15389d1a0ceb9e24580886ed9f890c09f528cc2
Samlet Wu <xiaofei.wu@gmail.com>
2021-05-07 02:26:54 +0800

    add some test units

---
+++ charts/src/main/java/com/bluecc/ws/charts/stuffs/Main.java
@@ -0,0 +1,57 @@
+package com.bluecc.ws.charts.stuffs;
+
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
+import com.linecorp.armeria.server.Server;
+import com.linecorp.armeria.server.ServerBuilder;
+import com.linecorp.armeria.server.docs.DocService;
+
+public class Main {
+
+    private static final Logger logger = LoggerFactory.getLogger(Main.class);
+
+    public static void main(String[] args) throws Exception {
+        final Server server = newServer(1080);
+
+        Runtime.getRuntime().addShutdownHook(new Thread(() -> {
+            server.stop().join();
+            logger.info("Server has been stopped.");
+        }));
+
+        server.start().join();
+
+        logger.info("Server has been started. Serving DocService at http://127.0.0.1:{}/docs",
+                    server.activeLocalPort());
+    }
+
+    /**
+     * Returns a new {@link Server} instance configured with annotated HTTP services.
+     *
+     * @param port the port that the server is to be bound to
+     */
+    static Server newServer(int port) {
+        final ServerBuilder sb = Server.builder();
+        return sb.http(port)
+                 .annotatedService("/pathPattern", new PathPatternService())
+                 .annotatedService("/injection", new InjectionService())
+                 .annotatedService("/messageConverter", new MessageConverterService())
+                 .annotatedService("/exception", new ExceptionHandlerService())
+                 .serviceUnder("/docs",
+                               DocService.builder()
+                                         .examplePaths(PathPatternService.class,
+                                                       "pathsVar",
+                                                       "/pathPattern/paths/first/foo",
+                                                       "/pathPattern/paths/second/bar")
+                                         .examplePaths(PathPatternService.class,
+                                                       "pathVar",
+                                                       "/pathPattern/path/foo",
+                                                       "/pathPattern/path/bar")
+                                         .exampleRequests(MessageConverterService.class,
+                                                          "json1",
+                                                          "{\"name\":\"bar\"}"
+                                         )
+                                         .build())
+                 .build();
+    }
+}
---
+++ justfile
@@ -0,0 +1,11 @@
+run:
+	# ./gradlew run
+	./gradlew run -PchooseMain=com.bluecc.ws.charts.stuffs.Main
+srv:
+	./gradlew run -PchooseMain=com.bluecc.ws.charts.SimpleClient
+simple:
+	./gradlew runSimple
+
+
+
+
